0bbcb1529d5815ba035cf2d8346a37d1235a00b2
Jeremy Ng <resumisg@gmail.com>
2021-03-26 18:06:39 +0800

    static server

--- webpack.server.js
+++ webpack.server.js
@@ -1,24 +1,28 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
-
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-  entry: './server/index.js',
+  entry: "./server/index.js",
 
-  target: 'node',
+  target: "node",
 
   externals: [nodeExternals()],
 
   output: {
-    path: path.resolve('server-build'),
-    filename: 'index.js'
+    path: path.resolve("server-build"),
+    filename: "index.js",
   },
-
+  plugins: [new MiniCssExtractPlugin()],
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader'
-      }
-    ]
-  }
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
 };
